6149e7ab1eaae9c6b3d7440052e43a1890c78864
dingwapi123 <1054757289@qq.com>
2022-08-08 22:31:13 +0800

    第一天

--- src/Hello.java
+++ src/Hello.java
@@ -1,5 +1,20 @@
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("hello world");
+        System.out.println("陈信至\t正在学习\njava");
+
+    }
+}
+
+class Dog{
+    public static void main(String[] args) {
+        System.out.println("dog");
+    }
+}
+
+
+
+class Tiger{
+    public static void main(String[] args) {
+        System.out.println("tiger");
     }
 }
---
+++ src/test1.java
@@ -0,0 +1,10 @@
+/**
+ * date: 2022/8/8 22:18
+ * author: cxz
+ * version: JDK 1.8
+ */
+public class test1 {
+    public static void main(String[] args) {
+        System.out.println("ok ok");
+    }
+}
